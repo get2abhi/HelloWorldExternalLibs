96937918e99334b1de230c092e9c904e8c87e9fc
Abhinav Srivastava <abhinav.srivastava@olacabs.com>
2018-05-07 16:19:31 +0530

    working static library gmath with gpower method

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,5 +1,6 @@
 #include <jni.h>
 #include <string>
+#include <gmath.h>
 
 extern "C"
 JNIEXPORT jstring
@@ -8,6 +9,7 @@ JNICALL
 Java_farzi_labs_helloworldexternallibs_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+    std::string result = std::to_string(gpower(4));
+    std::string hello = "The square of 4 is : "+result;
     return env->NewStringUTF(hello.c_str());
 }
