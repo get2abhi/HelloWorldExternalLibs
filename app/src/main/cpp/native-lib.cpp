dbb069fbf4b67919ceb55a49849f819a1d010163
Abhinav Srivastava <abhinav.srivastava@olacabs.com>
2018-05-09 16:02:21 +0530

    added shared so lib

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,6 +1,7 @@
 #include <jni.h>
 #include <string>
 #include <gmath.h>
+#include <add.h>
 
 extern "C"
 JNIEXPORT jstring
@@ -10,6 +11,6 @@ Java_farzi_labs_helloworldexternallibs_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
     std::string result = std::to_string(gpower(4));
-    std::string hello = "The square of 4 is : "+result;
+    std::string hello = "The square of 4 is "+result + " and sum of 2+3 = " + std::to_string(add(2,3));
     return env->NewStringUTF(hello.c_str());
 }
